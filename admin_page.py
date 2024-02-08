93a6f51b4a9a2f89a31a007d3899e6d6c17a6085
Ganesh23105 <ganeshpandhre2005@gmail.com>
2024-02-09 01:47:19 +0530

    wed

--- admin_page.py
+++ admin_page.py
@@ -4,12 +4,24 @@ from tkinter import ttk
 import pymysql
 from PIL import Image, ImageTk
 from tkcalendar import DateEntry
+from tkinter import filedialog
 
 
 
 def going_to_add():
+
+    def upload_photo():
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            image = Image.open(file_path)
+            image.thumbnail((50, 50))  
+            photo = ImageTk.PhotoImage(image)
+            photo_label.config(image=photo)
+            photo_label.image = photo
+
+
     admin_page_right_frame=Frame(admin_page,bg="blue")
-    admin_page_right_frame.grid(row=0,column=6)
+    admin_page_right_frame.grid(row=0,column=8)
 
     # product_Lframe = LabelFrame(left_frame, text="Products",font=("Poppins",12,"bold"))
     # product_Lframe.grid(row=0,column=0,pady=10,padx=10,sticky="nsew")
@@ -41,6 +53,30 @@ def going_to_add():
     birth_date = DateEntry(personal_details_Lframe)
     birth_date.grid(row=3,column=1)
 
+    age_label=Label(personal_details_Lframe,text="AGE")
+    age_label.grid(row=4,column=0)
+    age_entry=Entry(personal_details_Lframe)
+    age_entry.grid(row=4,column=1)
+
+    contact_no_label=Label(personal_details_Lframe,text="CONTACT NO")
+    contact_no_label.grid(row=5,column=0)
+    contact_no_entry=Entry(personal_details_Lframe)
+    contact_no_entry.grid(row=5,column=1)
+
+    email_address_label=Label(personal_details_Lframe,text="EMAIL")
+    email_address_label.grid(row=6,column=0)
+    email_address_entry=Entry(personal_details_Lframe)
+    email_address_entry.grid(row=6,column=1)
+
+    # photo_label=Label(personal_details_Lframe,text="PASSPORT PHOTO")
+    # photo_label.grid(row=7, column=0)
+    photo_label = Label(personal_details_Lframe,text="PASSPORT PHOTO")
+    photo_label.grid(row=7,column=0)
+    upload_button=Button(personal_details_Lframe,text="UPLOAD",command=upload_photo)
+    upload_button.grid(row=7,column=1)
+
+    submit_button=Button(personal_details_Lframe,text="SUBMIT")
+    submit_button.grid(row=8,columnspan=2)
 
 
 admin_page=Tk()
@@ -61,7 +97,6 @@ admin_page_search_button.grid(row=1,column=0)
 
 # admin_page_right_frame=Frame(admin_page,bg="blue")
 # admin_page_right_frame.grid(row=0,column=6)
-
 
 # admin_page_search_button=Button(admin_page_right_frame,text="hi")
 # admin_page_search_button.grid(row=0,column=0)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
-b
-=======
-e one",command=register,bd=0,font=("Times New Roman",15,"bold"),bg="white",fg="#163246",activeforeground='#373737',cursor="hand2",activebackground="white")
-# register_page_button.grid(row=4,column=0,pady=20,columnspan=2)
->>>>>>> 58ff93e4d8c44e4471232aed5c9e51d093faaf78
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        image = Image.open(file_path)
+        image.thumbnail((300, 300))  # Resize the image
+        photo = ImageTk.PhotoImage(image)
+        label.config(image=photo)
+        label.image = photo
