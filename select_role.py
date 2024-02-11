4a18b47efcffb26af02ddda6912b9e9ab526a86c
Yash-Naikwadi <yash.naikwadi2004@gmail.com>
2024-02-11 23:25:03 +0530

    user

--- select_role.py
+++ select_role.py
@@ -1,7 +1,5 @@
-
 from tkinter import *
-from tkinter import messagebox
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
 def employee_page():
     select_role.destroy()
@@ -11,20 +9,52 @@ def admin_page():
      select_role.destroy()
      import admin_login_registration
 
-select_role=Tk()
-select_role.title("WAREHOUSE")
-select_role.geometry("1200x675")
-select_role.resizable(0,0)
+def fill_image(event):
+	global resized_tk
 
-bgImage = ImageTk.PhotoImage(file='image\\select_role.jpg')
-bgLabel=Label(select_role,image= bgImage)
-bgLabel.place(x=0, y=0)
+	# current ratio 
+	canvas_ratio = event.width / event.height
+
+	# get coordinates 
+	if canvas_ratio > image_ratio: # canvas is wider than the image
+		width = int(event.width) 
+		height = int(width / image_ratio)
+	else: # canvas is narrower than the image
+		height = int(event.height)
+		width = int(height * image_ratio) 
+
+	resized_image = image_original.resize((width, height))
+	resized_tk = ImageTk.PhotoImage(resized_image)
+	canvas.create_image(
+		int(event.width / 2),
+		int(event.height / 2),
+		anchor = 'center',
+		image = resized_tk)
+
+select_role = Tk()
+select_role.geometry('1200x650')
+select_role.title('Images')
+select_role.minsize(880,495)
+
+# grid layout
+select_role.columnconfigure(0, weight = 1, uniform = 'a')
+select_role.rowconfigure(0, weight = 1)
+
+# import an image 
+image_original = Image.open('image\\select_role.jpg')
+image_ratio = image_original.size[0] / image_original.size[1]
+image_tk = ImageTk.PhotoImage(image_original)
+
+canvas = Canvas(select_role, background = 'black', bd = 0, highlightthickness = 0, relief = 'ridge')
+canvas.grid(column = 0, row = 0, sticky = 'nsew')
+
+canvas.bind('<Configure>', fill_image)
 
 sample_label=Label(select_role,background="white")
-sample_label.place(x=450,y=200)
+sample_label.place(relx=0.75,rely=0.5,anchor='center')
 
-sample_label2=Label(sample_label,text="SELECT A ROLE",bg="white",font=("Times New Roman",40,"bold"),fg="#373737")
-sample_label2.grid(row=0,column=0,padx=5)
+select_role_name_label=Label(sample_label,text="SELECT A ROLE",bg="white",font=("Times New Roman",40,"bold"),fg="#373737")
+select_role_name_label.grid(row=0,column=0,padx=5)
 
 role_admin_button=Button(sample_label,text="EMPLOYEE",command=employee_page, bd=0,font=("Times New Roman",25,"bold"),bg="#373737",cursor="hand2",fg="white",activeforeground='#373737')
 role_admin_button.grid(row=1,column=0,padx=50,pady=20)
@@ -32,4 +62,5 @@ role_admin_button.grid(row=1,column=0,padx=50,pady=20)
 role_staff_button=Button(sample_label,text="ADMIN",command=admin_page, bd=0,font=("Times New Roman",25,"bold"),width=10,bg="#373737",fg="white",activeforeground='#373737')
 role_staff_button.grid(row=3,column=0,padx=50,pady=20)
 
+# run
 select_role.mainloop()
